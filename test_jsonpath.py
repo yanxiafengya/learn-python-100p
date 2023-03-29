4dc857c12178d741ab117ddf0ae9e88e1ab34682
xiaobingaichongfeng <xiaochaoqi5820@163.com>
2023-03-29 22:05:05 +0800

    add test_jsonpath

---
+++ test_jsonpath.py
@@ -0,0 +1,34 @@
+import json
+import jsonpath
+import certifi
+import pygame
+
+
+data = '''{
+  "store": {
+    "book":[
+      { "category": "reference",
+        "author": "Nigel Rees",
+        "title": "Sayings of the Century",
+        "price": 8.95
+      },
+      { "category": "fiction",
+        "author": "J. R. R. Tolkien",
+        "title": "The Lord of the Rings",
+        "isbn": "0-395-19395-8",
+        "price": 22.99
+      }
+    ],
+    "bicycle": {
+      "color": "red",
+      "price": 19.95
+    }
+  }
+}'''
+
+data = json.loads(data)
+# print(data)
+
+print(jsonpath.jsonpath(data,'$..color'))
+print(jsonpath.jsonpath(data,'$..price'))
+
